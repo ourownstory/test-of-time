a5a2814f611c257872823ce9f039248c38f7c4f8
ourownstory <ourownstory@users.noreply.github.com>
2022-08-10 16:25:31 -0700

    init file

---
+++ deja-vu/__init__.py
@@ -0,0 +1,32 @@
+import logging
+
+log = logging.getLogger("dv")
+log.setLevel("INFO")
+
+c_handler = logging.StreamHandler()
+# c_handler.setLevel("WARNING")
+c_format = logging.Formatter("%(levelname)s - (%(name)s.%(funcName)s) - %(message)s")
+c_handler.setFormatter(c_format)
+log.addHandler(c_handler)
+
+logging.captureWarnings(True)
+warnings_log = logging.getLogger("py.warnings")
+warnings_log.addHandler(c_handler)
+
+write_log_file = False
+if write_log_file:
+    f_handler = logging.FileHandler("logs.log", "w+")
+    # f_handler.setLevel("ERROR")
+    f_format = logging.Formatter("%(asctime)s; %(levelname)s; %(name)s; %(funcName)s; %(message)s")
+    f_handler.setFormatter(f_format)
+    log.addHandler(f_handler)
+    warnings_log.addHandler(f_handler)
+
+# make version number accessible
+from ._version import __version__
+
+# make classes available upon package import
+from .benchmark import Dataset
+from .benchmark import NeuralProphetModel, ProphetModel
+from .benchmark import SimpleExperiment, CrossValidationExperiment
+from .benchmark import ManualBenchmark, ManualCVBenchmark, SimpleBenchmark, CrossValidationBenchmark
