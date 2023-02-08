7eae282005f774af6dcafbfb7059097c155bd50c
Leonie Freisinger <ge79kej@mytum.de>
2023-02-07 19:55:37 -0800

    add dataset loader. no interface changes.

--- tot/datasets/__init__.py
+++ tot/datasets/__init__.py
@@ -1,2 +1,95 @@
 # make classes available upon package import
 from .dataset import Dataset  # noqa: F401
+
+"""
+Datasets
+--------
+Popular datasets. Lists all datasets that are available. For adding a new dataset, setup a new instance and
+fill respective information.
+"""
+
+# imports
+from dataclasses import dataclass
+
+# make classes available upon package import
+from .dataset import Dataset  # noqa: F401
+from .dataset_loader import DatasetLoaderCSV, DatasetLoaderTSF, DatasetMetadataLoader
+
+"""
+    Overall usage of this package:
+    from tot.datasets import AirPassengersDataset
+    time series: df = AirPassengersDataset.load()
+"""
+# TODO: add logging
+# logger = get_logger(__name__)
+
+
+# example for loading local .csv
+@dataclass
+class AirPassengersDataset(DatasetLoaderCSV):
+    """
+    Monthly Air Passengers Dataset, from 1949 to 1960.
+    References
+    ----------
+    .. [1] # TODO: add reference
+    """
+
+    metadata: DatasetMetadataLoader = DatasetMetadataLoader(
+        name="air_passengers.csv",
+        freq="MS",
+        start_timestamp="1949-01-01",
+        horizon=3,
+        missing=False,
+        equallength=True,
+        multivariate=False,
+        seasonality_mode="multiplicative",
+    )
+
+
+# example for downloading .csv
+@dataclass
+class AusBeerDataset(DatasetLoaderCSV):
+    """
+    Total quarterly beer production in Australia (in megalitres) from 1956:Q1 to 2008:Q3 [1]_.
+    References
+    ----------
+    .. [1] https://rdrr.io/cran/fpp/man/ausbeer.html
+    """
+
+    metadata: DatasetMetadataLoader = DatasetMetadataLoader(
+        "ausbeer.csv",
+        url="https://raw.githubusercontent.com/unit8co/darts/master/datasets/ausbeer.csv",
+        header_time="date",
+        format_time="%Y-%m-%d",
+    )
+
+
+# example for downloading .tsf
+@dataclass
+class SunspotsNoMissing(DatasetLoaderTSF):
+    """
+    Contains the single daily time series representing the sunspot numbers from 08/01/1818 to 31/05/2020. As the
+    dataset contains missing values, a LOCF-imputed version is included.
+    ----------
+    .. [1] http://doi.org/10.5281/zenodo.4654722
+    """
+
+    metadata: DatasetMetadataLoader = DatasetMetadataLoader(
+        name="sunspot_dataset_without_missing_values.tsf",
+        url="https://zenodo.org/record/4654722/files/sunspot_dataset_without_missing_values.zip",
+    )
+
+
+# example for loading local .tsf
+@dataclass
+class TourismYearly(DatasetLoaderTSF):
+    """
+    This dataset originates from a Kaggle competition and contains 518 yearly time series related to tourism.
+    ----------
+    .. [1] http://doi.org/10.5281/zenodo.4656103
+    """
+
+    metadata: DatasetMetadataLoader = DatasetMetadataLoader(
+        name="tourism_yearly_dataset.tsf",
+        # url="https://zenodo.org/record/4656103/files/tourism_yearly_dataset.zip",
+    )
--- tot/datasets/dataset.py
+++ tot/datasets/dataset.py
@@ -1,8 +1,10 @@
 import logging
 from dataclasses import dataclass, field
-from typing import List, Optional
+from typing import List, Optional, Tuple, Type
 
 import pandas as pd
+
+from .dataset_loader import DatasetLoaderCSV, DatasetMetadataLoader
 
 log = logging.getLogger("tot.dataset")
 
@@ -10,9 +12,8 @@ log = logging.getLogger("tot.dataset")
 @dataclass
 class Dataset:
     """
-    example use:
     >>> dataset = Dataset(
-    >>>     df = pd.read_csv('air_passengers.csv'),
+    >>>     dataset = AirPassengersDataset(),
     >>>     name = "air_passengers",
     >>>     freq = "MS",
     >>>     seasonalities = [365.25,], # yearly seasonality
@@ -20,8 +21,21 @@ class Dataset:
     >>> ),
     """
 
-    df: pd.DataFrame
-    name: str
-    freq: str
-    seasonalities: List = field(default_factory=list)
-    seasonality_mode: Optional[str] = None
+    df: pd.DataFrame  # only temporary
+    dataset: DatasetLoaderCSV = None
+    name: Optional[str] = None
+    freq: Optional[str] = None
+    seasonalities: Optional[List] = field(default_factory=list)
+    seasonality_mode: Optional[str] = "additive"
+    # TODO: add future attributes that need to be mapped to metadata
+
+    def __post_init__(self):
+        if self.dataset is None:
+            self.dataset = DatasetLoaderCSV(
+                metadata=DatasetMetadataLoader(
+                    name=self.name,
+                    freq=self.freq,
+                    seasonalities=self.seasonalities,
+                    seasonality_mode=self.seasonality_mode,
+                )
+            )
