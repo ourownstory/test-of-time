08bfeea29905fc19ccf750f0216c1c43f01a1bf7
Leonie Freisinger <ge79kej@mytum.de>
2023-02-14 17:22:54 -0800

    remove typing

--- tot/plot_utils.py
+++ tot/plot_utils.py
@@ -232,7 +232,7 @@ def _plot_plotly(
     ylabel: str = "y",
     highlight_forecast: Union[int, None] = None,
     figsize: tuple[int, int] = (700, 350),
-    resampler_active: bool = False,
+    resampler_active=False,
 ):
     """
     Plot the NeuralProphet forecast
