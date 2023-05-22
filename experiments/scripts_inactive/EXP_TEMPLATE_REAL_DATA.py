017425ed2d854164eee4ff7bbd9bf187b53ebcd5
aime <freisinger@tueiewk-lssim65.ewk.ei.tum.de>
2023-05-22 04:21:22 +0000

    commit deleted files

---
+++ experiments/scripts_inactive/EXP_TEMPLATE_REAL_DATA.py
@@ -0,0 +1,44 @@
+from sklearn.preprocessing import MinMaxScaler, PowerTransformer, QuantileTransformer, RobustScaler, StandardScaler
+
+from experiments.pipeline_experiment import run
+from experiments.utils import LogTransformer, load_EIA
+from tot.models import NeuralProphetModel
+
+PLOT = False
+DIR_NAME = "EIA"
+FREQ = "H"
+MODEL = NeuralProphetModel
+MODEL_PARAMS = {
+    "n_forecasts": 1,
+    "epochs": 10,
+    "global_normalization": True,
+    "normalize": "off",
+    "n_lags": 24,
+}
+
+scalers = [
+    StandardScaler(),
+    MinMaxScaler(feature_range=(-1, 1)),
+    MinMaxScaler(feature_range=(0, 1)),
+    RobustScaler(quantile_range=(25, 75)),
+    PowerTransformer(method="box-cox", standardize=True),
+    PowerTransformer(method="yeo-johnson", standardize=True),
+    QuantileTransformer(output_distribution="normal"),
+    LogTransformer(),
+]
+
+run(
+    dir_name=DIR_NAME,
+    save=True,
+    df=load_EIA(),
+    df_name=DIR_NAME,
+    freq=FREQ,
+    model_class=MODEL,
+    model_params=MODEL_PARAMS,
+    scalers=scalers,
+    scaling_levels="default",
+    reweight_loss=True,
+    metrics=["MAE", "RMSE", "MAPE", "MASE"],
+    test_percentage=0.25,
+    plot=PLOT,
+)
