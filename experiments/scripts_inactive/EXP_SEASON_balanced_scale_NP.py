6e7872d21d1abc1e9ccfb0dcc7d139e86a147513
Leonie Freisinger <ge79kej@mytum.de>
2023-05-27 14:12:01 -0700

    adapt one exp

--- experiments/scripts_inactive/EXP_SEASON_balanced_scale_NP.py
+++ experiments/scripts_inactive/EXP_SEASON_balanced_scale_NP.py
@@ -1,43 +1,48 @@
 import pandas as pd
-
+import multiprocessing
 from experiments.pipeline_experiment import run
 from experiments.utils import generate_one_shape_season_data
 from tot.models import NeuralProphetModel
 
-PLOT = False
-DIR_NAME = "SEASON_balanced_scale_NP"
-FREQ = "H"
-SERIES_LENGTH = 24 * 7 * 15
-DATE_RNG = pd.date_range(start=pd.to_datetime("2011-01-01 01:00:00"), periods=SERIES_LENGTH, freq="H")
-MODEL = NeuralProphetModel
-MODEL_PARAMS = {
-    "n_forecasts": 1,
-    "epochs": 30,
-    "global_normalization": True,
-    "normalize": "off",
-    "n_lags": 4,
-}
+def run_benchmark():
+    PLOT = False
+    DIR_NAME = "SEASON_balanced_scale_NP"
+    FREQ = "H"
+    SERIES_LENGTH = 24 * 7 * 15
+    DATE_RNG = pd.date_range(start=pd.to_datetime("2011-01-01 01:00:00"), periods=SERIES_LENGTH, freq="H")
+    MODEL = NeuralProphetModel
+    MODEL_PARAMS = {
+        "n_forecasts": 1,
+        "epochs": 30,
+        "global_normalization": True,
+        "normalize": "off",
+        "n_lags": 4,
+    }
 
-df = generate_one_shape_season_data(
-    series_length=SERIES_LENGTH,
-    date_rng=DATE_RNG,
-    n_ts_groups=[5, 5],
-    offset_per_group=[1000, 100],
-    amplitude_per_group=[50, 50],
-)
+    df = generate_one_shape_season_data(
+        series_length=SERIES_LENGTH,
+        date_rng=DATE_RNG,
+        n_ts_groups=[5, 5],
+        offset_per_group=[1000, 100],
+        amplitude_per_group=[50, 50],
+    )
 
-run(
-    dir_name=DIR_NAME,
-    save=True,
-    df=df,
-    df_name=DIR_NAME,
-    freq=FREQ,
-    model_class=MODEL,
-    model_params=MODEL_PARAMS,
-    scalers="default",
-    scaling_levels="default",
-    reweight_loss=True,
-    metrics=["MAE", "RMSE", "MAPE", "MASE"],
-    test_percentage=0.25,
-    plot=PLOT,
-)
+    run(
+        dir_name=DIR_NAME,
+        save=True,
+        df=df,
+        df_name=DIR_NAME,
+        freq=FREQ,
+        model_class=MODEL,
+        model_params=MODEL_PARAMS,
+        scalers="default",
+        scaling_levels="default",
+        reweight_loss=True,
+        metrics=["MAE", "RMSE", "MAPE", "MASE"],
+        test_percentage=0.25,
+        plot=PLOT,
+    )
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    run_benchmark()
