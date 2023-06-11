f0cff63c75b5cb9229f805a6aec5d1d111a80c94
Leonie Freisinger <ge79kej@mytum.de>
2023-06-11 06:35:02 -0700

    correct funnc

--- experiments/WINDOW_NP_exe_sherlock.sh
+++ experiments/WINDOW_NP_exe_sherlock.sh
@@ -3,11 +3,11 @@
 #!/bin/bash
 
 # load modules
-module load python/3.9.0
-module load cuda/11.7.1
+#module load python/3.9.0
+#module load cuda/11.7.1
 
 # activate the virtual environment
-source ../../tot4/bin/activate
+#source ../../tot4/bin/activate
 
 # re-install the packages
 pip uninstall -y neuralprophet
@@ -16,6 +16,10 @@ pip install -e git+https://github.com/LeonieFreisinger/darts.git@revin_learnable
 
 #git checkout evolution_experiments_1
 #git pull upstream evolution_experiments_1
+
+nohup python experiments/EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0.3,0.03" --gen_func gen_model_and_params_norm > outfile1
+nohup python experiments/EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model NeuralProphetModel --params NP_FNN --data_n_ts_groups "5,5" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0.3,0.03" --gen_func gen_model_and_params_norm > outfile2
+
 
 ### NP_FNN SEASON
 commands_NP_FNN_SEASON=(
@@ -48,9 +52,9 @@ commands_NP_FNN_TREND=(
 'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model NeuralProphetModel --params NP_FNN --data_n_ts_groups "5,5" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0.3,0.03" --gen_func gen_model_and_params_norm'
 'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model NeuralProphetModel --params NP_FNN --data_n_ts_groups "1,10" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0.3,0" --gen_func gen_model_and_params_norm'
 'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model NeuralProphetModel --params NP_FNN --data_n_ts_groups "10,1" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,50" --data_trend_gradient_per_group "0.3,0" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model NeuralProphetModel --params NP_FNN --data_n_ts_groups "1,10" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0,0.03"--gen_func gen_model_and_params_norm '
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model NeuralProphetModel --params NP_FNN --data_n_ts_groups "1,10" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0,0.03" --gen_func gen_model_and_params_norm '
 'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model NeuralProphetModel --params NP_FNN --data_n_ts_groups "1,10" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0,0.03" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model NeuralProphetModel --params NP_FNN --data_n_ts_groups "2,2,2,2" --data_offset_per_group "1000,1000, 100, 100" --data_amplitude_per_group "50,50,5,5" --data_trend_gradient_per_group "0.3,0,0.03,0" --gen_func gen_model_and_params_norm'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model NeuralProphetModel --params NP_FNN --data_n_ts_groups "2,2,2,2" --data_offset_per_group "1000,1000, 100, 100" --data_amplitude_per_group "50,50,5,5" --data_trend_gradient_per_group 0"0.3,0,0.03,0" --gen_func gen_model_and_params_norm'
 'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model NeuralProphetModel --params NP_FNN --data_n_ts_groups "1,1,2,2" --data_offset_per_group "1000,1000, 100, 100" --data_amplitude_per_group "50,50,5,5" --data_trend_gradient_per_group "0.3,0,0.03,0" --gen_func gen_model_and_params_norm'
 )
 
@@ -58,13 +62,13 @@ commands_NP_FNN_TREND=(
 
 ### RNN SEASON
 commands_RNN_SEASON=(
- 'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_data --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "0,0" --data_amplitude_per_group "50,50" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_data --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "0,0" --data_amplitude_per_group "50,5" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_data --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,50" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_data --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_data_with_outlier --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_intermittent --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "0,0" --data_amplitude_per_group "50,5" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_data --model RNNModel --params RNN --data_n_ts_groups "2,2,2,2" --data_offset_per_group "100,1000,100,1000" --data_amplitude_per_group "50,50,5,5" --gen_func gen_model_and_params_norm'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_data --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "0,0" --data_amplitude_per_group "50,50" --gen_func gen_model_and_params_none'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_data --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "0,0" --data_amplitude_per_group "50,5" --gen_func gen_model_and_params_none'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_data --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,50" --gen_func gen_model_and_params_none'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_data --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --gen_func gen_model_and_params_none'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_data_with_outlier --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --gen_func gen_model_and_params_none'
+'python3 EXP_SYN_DATA.py --data_func generate_intermittent --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "0,0" --data_amplitude_per_group "50,5" --gen_func gen_model_and_params_none'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_data --model RNNModel --params RNN --data_n_ts_groups "2,2,2,2" --data_offset_per_group "100,1000,100,1000" --data_amplitude_per_group "50,50,5,5" --gen_func gen_model_and_params_none'
 )
 
 ### RNN SEASHAPE
@@ -84,13 +88,13 @@ commands_RNN_SEASHAPE=(
 
 ### RNN TREND
 commands_RNN_TREND=(
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0.3,0.03" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "1,10" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0.3,0" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "10,1" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,50" --data_trend_gradient_per_group "0.3,0" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "1,10" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0,0.03" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "1,10" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0,0.03" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "2,2,2,2" --data_offset_per_group "1000,1000, 100, 100" --data_amplitude_per_group "50,50,5,5" --data_trend_gradient_per_group "0.3,0,0.03,0" --gen_func gen_model_and_params_norm'
-'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "1,1,2,2" --data_offset_per_group "1000,1000, 100, 100" --data_amplitude_per_group "50,50,5,5" --data_trend_gradient_per_group "0.3,0,0.03,0" --gen_func gen_model_and_params_norm'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "5,5" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0.3,0.03" --gen_func gen_model_and_params_none'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "1,10" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0.3,0" --gen_func gen_model_and_params_none'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "10,1" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,50" --data_trend_gradient_per_group "0.3,0" --gen_func gen_model_and_params_none'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "1,10" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0,0.03" --gen_func gen_model_and_params_none'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "1,10" --data_offset_per_group "1000,100" --data_amplitude_per_group "50,5" --data_trend_gradient_per_group "0,0.03" --gen_func gen_model_and_params_none'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "2,2,2,2" --data_offset_per_group "1000,1000, 100, 100" --data_amplitude_per_group "50,50,5,5" --data_trend_gradient_per_group "0.3,0,0.03,0" --gen_func gen_model_and_params_none'
+'python3 EXP_SYN_DATA.py --data_func generate_one_shape_season_and_ar_and_trend_data --model RNNModel --params RNN --data_n_ts_groups "1,1,2,2" --data_offset_per_group "1000,1000, 100, 100" --data_amplitude_per_group "50,50,5,5" --data_trend_gradient_per_group "0.3,0,0.03,0" --gen_func gen_model_and_params_none'
 )
 
 
